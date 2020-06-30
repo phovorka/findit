e34c17f58901712fa3a6832a6429e0bfbce1d391
phovorka <67637867+phovorka@users.noreply.github.com>
2020-06-30 14:23:25 +0200

    Create findit.js

---
+++ findit.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+ var params  = 'width='+(((screen.width)/2));
+ params += ', height='+screen.height;
+ params += ', top=0';
+ params += ', left='+(((screen.width)/2)-10);
+ params += ', fullscreen=yes';
+
+  $("#btn1").click(function(){
+   var q = $("#test3").val(); 
+   var url1 = "https://codepen.io/search/pens?q=";
+   var url = url1+q;
+   window.open(url, 'window name', params);
+   return false;
+    });
+  
+    $("#btn1").click(function(){
+   var q = $("#test3").val(); 
+   var url1 = "https://www.google.com/search?q=";
+   var url = url1+q;
+   window.open(url, 'window name', params);
+   return false;
+    });
+
+  });
